64b3f6a06514cdf128a79cf30f82963dad162bd7
svoronkov <svoronkov@kaaiot.io>
2021-07-10 18:54:58 +0300

    "added nested loops"

---
+++ loops/nestedloops.py
@@ -0,0 +1,10 @@
+# for i in range(10):
+#    for j in range(30):
+#        print('*', end='')
+#     print()
+
+
+for row in range(5):
+    for column in range(30):
+        print("*", end="")
+    print()
