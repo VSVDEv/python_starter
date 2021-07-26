46f583b61611a3007b8e8de998b6160cd3258236
svoronkov <svoronkov@kaaiot.io>
2021-07-26 20:08:56 +0300

    "added modules"

---
+++ modules/03_ModuleSearchPath/main.py
@@ -0,0 +1,30 @@
+﻿"""Пример использования sys.path
+
+При импортировании модулей интерпретатор Python ищет их в директориях и архивах,
+список которых доступен как для чтения, так и для модификации в виде переменной
+path встроенного модуля sys.
+"""
+
+import sys      # встроенный модуль sys
+import os.path  # функции для работы с путями в файловой системе
+
+print(sys.path)  # вывод списка путей поиска модулей
+
+# Получение имени директории текущего модуля.
+# __file__ -- путь к файлу текущего модуля
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+parent_path = os.path.dirname(current_dir)
+
+# Получение пути к первому примеру
+example_00_path = os.path.join(os.path.dirname(parent_path), '00_Module')
+
+# Добавление к пути поиска модулей
+sys.path.append(example_00_path)
+
+# Импортирование модуля fibonacci
+
+import fibonacci
+
+# Вызов его функции
+print(fibonacci.nth_fibonacci(20))
---
+++ modules/04_Packages/framework/models.py
@@ -0,0 +1,7 @@
+﻿"""Модуль эмуляции ORM"""
+
+
+class Model(object):
+    @classmethod
+    def get_objects(cls):
+        print('Retrieving ' + cls.__name__ + ' objects from DB')
